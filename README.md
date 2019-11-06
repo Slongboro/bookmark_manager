d5cf9fecb5d56d64206ad521cb46c81b6f82dd5c
slongboro <susanlongley@gmail.com>
2019-11-06 13:39:14 +0000

    upto date sections complete

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Set up the database 
 - to access = psql postgres
 - CREATE DATABASE bookmark_manager;
+- CREATE DATABASE "bookmark_manager_test";
 - connect to \c bookmark_manager;
 - CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
 handy tips
@@ -12,3 +13,7 @@ User Story
 As a busy internet user
 So that I can quickly access my favourite websites
 I would like to see a list of bookmarks
+
+As a user
+So I can store bookmark data for later retrieval
+I want to add a bookmark to Bookmark Manager
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,11 +1,15 @@
+require 'pg'
 
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
+    
+    visit('/bookmarks')
 
-    feature 'Viewing bookmarks' do
-        scenario 'A user can see bookmarks' do
-          visit('/bookmarks')
-      
-          expect(page).to have_content "http://www.makersacademy.com"
-          expect(page).to have_content "http://www.destroyallsoftware.com"
-          expect(page).to have_content "http://www.google.com"
-        end
-    end
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
+end
